4903b8cfd341b0e9559a746b50b359dd8674462f
Marcos Jara <marcos.jara@tecnoandina.cl>
2023-06-07 11:36:35 -0400

    Guardar socio listo

--- Controllers/partners/partnersC.php
+++ Controllers/partners/partnersC.php
@@ -1,7 +1,6 @@
 <?php
-
-
 require_once "../../Models/partners/partnersM.php";
+require_once "../helpers/validationsC.php";
 
 class PartnersC
 {
@@ -22,6 +21,144 @@ class PartnersC
     }
 
 
+    function savePartnerC(){
+
+        $_data = json_decode($_POST["data"], true);
+        $rut = $_data["rut"];
+        $firstName = $_data["firstName"];
+        $secondName = $_data["secondName"];
+        $lastname = $_data["lastname"];
+        $secondLastname = $_data["secondLastname"];
+        $birthdate = $_data["birthdate"];
+        $gender = $_data["gender"];
+        $educationalLevel = $_data["educationalLevel"];
+        $occupation = $_data["occupation"];
+        $admissionDate = $_data["admissionDate"];
+        $rol = $_data["rol"];
+        $cellPhone = $_data["cellPhone"];
+        $phone = $_data["phone"];
+        $mail = $_data["mail"];
+        $street = $_data["street"];
+        $number = $_data["number"];
+        $references = $_data["references"];
+        $regionId = $_data["regionId"];
+        $provinceId = $_data["provinceId"];
+        $communeId = $_data["communeId"];
+
+
+        $separateRut = explode('-', $rut);
+        $dv = $separateRut[1];
+        $rutSinPuntos = explode('.', $separateRut[0]);
+        $rutSave = $rutSinPuntos[0] . $rutSinPuntos[1] . $rutSinPuntos[2];
+
+
+
+        $dataSave = [
+            "rutSave" => $rutSave,
+            "dv" => $dv,
+            "firstName" => $firstName,
+            "secondName" => $secondName,
+            "lastname" => $lastname,
+            "secondLastname" => $secondLastname,
+            "birthdate" => $birthdate,
+            "gender" => $gender,
+            "educationalLevel" => $educationalLevel,
+            "occupation" => $occupation,
+            "admissionDate" => $admissionDate,
+            "rol" => $rol,
+            "cellPhone" => $cellPhone,
+            "phone" => $phone,
+            "mail" => $mail,
+            "street" => $street,
+            "number" => $number,
+            "references" => $references,
+            "regionId" => $regionId,
+            "provinceId" => $provinceId,
+            "communeId" => $communeId,
+        ];
+
+        //VALIDACIONES
+        $errors = [];
+        foreach($dataSave as $key => $value){
+            switch($key){
+                case "rutSave":
+                    $error = ValidationsC::validateEmptyString($value, "Rut");
+                    if($error != null){
+                        $errors[] = $error;
+                    }
+                    break;
+                case "firstName":
+                    $error =  ValidationsC::validateEmptyString($value, "Primer Nombre");
+                     if($error != null){
+                        $errors[] = $error;
+                    }
+                    break;
+                case "lastname":
+                    $error = ValidationsC::validateEmptyString($value, "Apellido Paterno");
+                     if($error != null){
+                        $errors[] = $error;
+                    }
+                    break;
+                case "street":
+                    $error = ValidationsC::validateEmptyString($value, "Calle");
+                     if($error != null){
+                        $errors[] = $error;
+                    }
+                    break;
+                case "number":
+                    $error =  ValidationsC::validateEmptyString($value, "Número");
+                     if($error != null){
+                        $errors[] = $error;
+                    }
+                    break;
+                case "idRegion":
+                    $error =  ValidationsC::validateSelection($value, "Región");
+                     if($error != null){
+                        $errors[] = $error;
+                    }
+                    break;
+                case "idProvincia":
+                    $error =   ValidationsC::validateSelection($value, "Provincia");
+                     if($error != null){
+                        $errors[] = $error;
+                    }
+                    break;
+                case "idComuna":
+                    $error = ValidationsC::validateSelection($value, "Comuna");
+                     if($error != null){
+                        $errors[] = $error;
+                    }
+                    break;
+
+                case "cellPhone":
+                    $error = ValidationsC::validateEmptyString($value, "Celular");
+                    if($error != null){
+                        $errors[] = $error;
+                    }
+                    break;
+                
+                case "mail":
+                    $error = ValidationsC::validateEmptyString($value, "Correo");
+                    if($error != null){
+                        $errors[] = $error;
+                    }
+                    break;
+            }
+        }
+
+
+      if(!empty($errors)){
+          echo json_encode(["errors"=>$errors]);
+          return;
+      }
+
+     $response = PartnersM::savePartnerM($dataSave);
+     echo json_encode($response);
+
+
+    }
+
+
 }
 
 
@@ -38,6 +175,6 @@ if ($_POST["action"] == "getPartners") {
 } elseif($_POST["action"] == "savePartner") {
 
     $action = new PartnersC;
-    $action->getEducationalLevelC();
+    $action->savePartnerC();
 
 } 
--- Models/partners/partnersM.php
+++ Models/partners/partnersM.php
@@ -4,9 +4,6 @@ require_once "../../Models/conexionBD.php";
 
 class PartnersM extends conexionBD
 {
-
-
-
     static function getPartnersM()
     {
         try {
@@ -38,5 +35,81 @@ class PartnersM extends conexionBD
 
     }
 
+    static function savePartnerM($dataSave){
+
+
+        $conection = conexionBD::cBD();
+
+        try {
+
+            $conection->beginTransaction();
+
+            //INSERT TABLA DIRECCION
+
+            $sqlDireccion = "INSERT INTO tj_direccion (calle, numero, referencia, region_fk, provincia_fk, comuna_fk) 
+                            values (:calle, :numero, :referencia, :region, :provincia, :comuna)";
+
+            $pdo = $conection->prepare($sqlDireccion);
+            $pdo->bindParam(":calle", $dataSave['street'], PDO::PARAM_STR );
+            $pdo->bindParam(":numero", $dataSave['number'], PDO::PARAM_STR );
+            $pdo->bindParam(":referencia", $dataSave['references'], PDO::PARAM_STR );
+            $pdo->bindParam(":region", $dataSave['regionId'], PDO::PARAM_INT );
+            $pdo->bindParam(":provincia", $dataSave['provinceId'], PDO::PARAM_INT );
+            $pdo->bindParam(":comuna", $dataSave['communeId'], PDO::PARAM_INT );
+            $pdo->execute();
+            $idDireccion = $conection->lastInsertId();
+
+            if ($idDireccion == NULL) {
+                $conection->rollBack();
+                return array("state" => false, "data" => "Error al insertar la direccion");
+            }
+
+            $sqlSavePartner = "INSERT INTO tj_persona (rut_persona, dv_persona, primer_nombre, segundo_nombre, apellido_paterno,
+            apellido_materno,  fecha_nacimiento, ocupacion_persona, fecha_ingreso, rol_persona, 
+            genero_persona, celular, telefono, correo, id_estudios_fk, id_direccion_fk)
+            values(:rut, :dv, :primerNombre, :segundoNombre, :apellidoPaterno, :apellidoMaterno, :fechaNacimiento, :ocupacionPersona,
+            :fechaIngreso, :rolPersona, :generoPersona, :celular, :telefono, :correo, :idEstudio, :idDireccion)";
+
+            $pdo = $conection->prepare($sqlSavePartner);
+            $pdo->bindParam(":rut", $dataSave['rutSave'], PDO::PARAM_INT);
+            $pdo->bindParam(":dv", $dataSave['dv'], PDO::PARAM_STR );
+            $pdo->bindParam(":primerNombre", $dataSave['firstName'], PDO::PARAM_STR );
+            $pdo->bindParam(":segundoNombre", $dataSave['secondName'], PDO::PARAM_STR );
+            $pdo->bindParam(":apellidoPaterno", $dataSave['lastname'], PDO::PARAM_STR );
+            $pdo->bindParam(":apellidoMaterno", $dataSave['secondLastname'], PDO::PARAM_STR );
+            $pdo->bindParam(":fechaNacimiento", $dataSave['birthdate'], PDO::PARAM_STR );
+            $pdo->bindParam(":ocupacionPersona", $dataSave['occupation'], PDO::PARAM_STR );
+            $pdo->bindParam(":fechaIngreso", $dataSave['admissionDate'], PDO::PARAM_STR );
+            $pdo->bindParam(":rolPersona", $dataSave['rol'], PDO::PARAM_STR );
+            $pdo->bindParam(":generoPersona", $dataSave['gender'], PDO::PARAM_STR );
+            $pdo->bindParam(":celular", $dataSave['cellPhone'], PDO::PARAM_STR );
+            $pdo->bindParam(":telefono", $dataSave['phone'], PDO::PARAM_STR );
+            $pdo->bindParam(":correo", $dataSave['mail'], PDO::PARAM_STR );
+            $pdo->bindParam(":idEstudio", $dataSave['educationalLevel'], PDO::PARAM_STR );
+            $pdo->bindParam(":idDireccion", $idDireccion, PDO::PARAM_INT );
+          
+
+            if ($pdo->execute()) {
+                $conection->commit();
+                return ["state" => true, "data" => "Registro guardado con satisfactoriamente"];
+
+            }
+
+
+            $conection->rollBack();
+            return ["state" => false, "data" => "Hubo un problema al intentar guardar el registo, si el problema persiste contacte al administrador"];
+
+
+        } catch (PDOException $error) {
+            $conection->rollBack();
+            return ["state" => false, "data" => $error->getMessage()];
+        }
+
+
+
+
+
+
+    }
 
 }
